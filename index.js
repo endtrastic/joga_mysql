048766f5ea31f36b0f5ab3ec64fc7ce9b32d450a
endtrastic <a.tuvikene@voco.ee>
2024-09-30 11:54:44 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const path = require('path');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
 const app = express();
-const express = require('express');
+
+const path = require('path');
 
 const hbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
@@ -15,9 +12,7 @@ app.engine('hbs', hbs.engine({
     layoutsDir: __dirname + '/views/layouts/',
 }));
 
-app.use(express.static('public'))
-
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,4 +27,19 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected to joga_mysql db");
+});
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+      if (err) throw err;
+      articles = result
+      console.log(articles)
+      res.render('index',  { articles: articles });
+    }) 
+})
+
+app.listen(3003, () => {
+  console.log('App is started at http://localhost:3003');
 });
