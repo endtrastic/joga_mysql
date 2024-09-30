eb76eba73487df49d37ea7b4093b0e3d44069310
endtrastic <a.tuvikene@voco.ee>
2024-09-30 11:24:12 +0300

    Added handlebars template engine

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
 const app = express();
+const express = require('express');
 
+const hbs = require('express-handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+}));
+
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'arm',
-  password: 'qwerty',
-  database: 'joga_mysql'
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "arm",
+    password: "qwerty",
+    database: "joga_mysql"
 });
 
-con.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to joga_mysql db');
-});
-
-app.listen(3003, () => {
-  console.log('App is started at http://localhost:3003');
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to joga_mysql db");
 });
